6db39f618a34762b3f8c7435c22a260a99e76820
Sasank Thapa <thapas@beloit.edu>
2021-09-13 15:08:44 -0500

    Quick Update

--- client/src/components/InfoOverlay/InfoOverlaw.js
+++ client/src/components/InfoOverlay/InfoOverlaw.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import classes from './InfoOverlay.module.css';
 
 const InfoOverlay = () =>{
-    return <div className={classes.mainContainer}>
+    const [show, setShow]=useState(true);
+
+    return <div className={show?classes.mainContainer:classes.doneContainer}>
+        {show?<>
+        <div className={classes.info}>
+            Not updated as of August 15th 2021.
+        </div>
+        <div className={classes.info}>
+            Please check out <a href=' https://covid19.ndrrma.gov.np/' target="_blank">{"https://covid19.ndrrma.gov.np/"}</a>
+        </div>
+        <div onClick={()=>setShow(setShow(false))} className={classes.info}>
+            Close
+        </div></>:
         <div className={classes.info}>
             Made with <span aria-label="heart" role="img">💚</span> by Sasank
-        </div>
+        </div>}
     </div>
 }
 
